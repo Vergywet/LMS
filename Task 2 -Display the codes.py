8a441b9ba9e32fc4a320e28def7fb4a23cafdd7b
Vergywet <136698806+Vergywet@users.noreply.github.com>
2024-03-23 23:50:40 +0200

    Update Task 2 -Display the codes.py

--- Task 2 -Display the codes.py
+++ Task 2 -Display the codes.py
@@ -1,11 +1,10 @@
-#Normal List
+#Normal  Comprehensive
 book_codes  = [14,15,16,17,18,19,20]
 myBook_codes = []
 for b in book_codes:
     myBook_codes.append(b)
-    print(f"Book codes :{myBook_codes}")
+    print("Book Codes : ",myBook_codes)
 
-#List Comprehension
-book_codes = [14,15,16,17,18,19,20]
+#List Comprehensive
 myBook_codes = [b for b in book_codes]
-print(f"Book codes are :{myBook_codes}")
+print("List Codes : ",myBook_codes)
